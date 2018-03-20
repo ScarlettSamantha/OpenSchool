2e936cc8958354e154a1ec65c321ffc1caebbcaa
Scarlett Verheul <scarlett@arcane-tech.nl>
2018-03-20 10:52:37 +0100

    [TASK] Added the 2 basic user fetch functions

--- responses/user.py
+++ responses/user.py
@@ -26,3 +26,10 @@ full_fields = {
     'date_created': fields.DateTime(dt_format='rfc822'),
     'date_updated': fields.DateTime(dt_format='rfc822')
 }
+
+index_fields = {
+    'id': fields.String,
+    'username': fields.String,
+    'firstname': fields.String,
+    'lastname': fields.String
+}
